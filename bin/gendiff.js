d12ce7e7e0c0b95d3b6a70ccf96ec174bf0ea46b
v1dgt <v1dgt@mail.ru>
2025-08-02 13:43:59 +0300

    add flag -F in gendiff

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import { program } from 'commander'
+
+program
+  .description('Compares two configuration files and shows a difference')
+  .version('1.0.0')
+  .arguments('<filepath1>')       
+  .arguments('<filepath2>')
+  .option('-f, --format [type]', 'output format')
+
+program.parse()

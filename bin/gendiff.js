bbc266493f882bc2991fba447410c79ded9c030c
v1dgt <v1dgt@mail.ru>
2025-08-06 06:08:47 +0300

    upd gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,21 @@
 #!/usr/bin/env node
 import { program } from 'commander'
+import { cwd } from 'node:process'
+import { resolve } from 'node:path'
+import parseJson from './gendiff-parse.js'
 
 program
   .description('Compares two configuration files and shows a difference')
   .version('1.0.0')
-  .arguments('<filepath1>')       
-  .arguments('<filepath2>')
-  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')       
+  .option('-f, --format [type]', 'output format (default: "stylish")')
 
+  .action((filepath1, filepath2, options) => {
+    const convertPath = (value) => value.startsWith('/') ? value : resolve(cwd(), value)
+    const path1 = convertPath(filepath1)
+    const path2 = convertPath(filepath2)
+    const file1 = parseJson(path1)
+    const file2 = parseJson(path2)
+  })
 program.parse()

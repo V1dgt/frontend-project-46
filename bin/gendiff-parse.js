0270b8645a4913dcae65a6bcb39edd649ec22181
v1dgt <v1dgt@mail.ru>
2025-08-07 16:50:11 +0300

    upd gendiff-parse.js

--- bin/gendiff-parse.js
+++ bin/gendiff-parse.js
@@ -1,7 +1,7 @@
-import {readFileSync } from 'node:fs'
+import { readFileSync } from 'node:fs'
 
-export default function(path) {
-    const file = readFileSync(path, 'utf-8')
-    const obj = JSON.parse(file)
-    return obj
+export default function (path) {
+  const file = readFileSync(path, 'utf-8')
+  const obj = JSON.parse(file)
+  return obj
 }

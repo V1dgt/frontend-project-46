5e415d98c2fb71e787fc7cd34981fb3bf85829fd
v1dgt <v1dgt@mail.ru>
2025-08-07 16:51:53 +0300

    add gendiff-comparison.js

---
+++ bin/gendiff-comparison.js
@@ -0,0 +1,28 @@
+export default function (file1, file2) {
+  const allKeys = [...Object.keys(file1), ...Object.keys(file2)]
+  const sortedUniqueKeys = [...new Set(allKeys)].sort()
+  const comparison = (keys) => {
+    const iter = (keys, acc) => {
+      if (keys.length === 0) {
+        return acc
+      }
+      const [currentKey, ...restKeys] = keys
+      if ((currentKey in file1) && !(currentKey in file2)) {
+        return iter(restKeys, [...acc, `  - ${currentKey}: ${file1[currentKey]}`])
+      }
+      else if (!(currentKey in file1) && (currentKey in file2)) {
+        return iter(restKeys, [...acc, `  + ${currentKey}: ${file2[currentKey]}`])
+      }
+      else if ((currentKey in file1) && (currentKey in file2) && file1[currentKey] === file2[currentKey]) {
+        return iter(restKeys, [...acc, `    ${currentKey}: ${file1[currentKey]}`])
+      }
+      else if ((currentKey in file1) && (currentKey in file2) && file1[currentKey] !== file2[currentKey]) {
+        return iter(restKeys, [...acc, `  - ${currentKey}: ${file1[currentKey]}`, `  + ${currentKey}: ${file1[currentKey]}`])
+      }
+    }
+    return iter(keys, [])
+  }
+  const filtredKeys = comparison(sortedUniqueKeys)
+  const result = filtredKeys.join('\n')
+  console.log(`{\n${result}\n}`)
+}

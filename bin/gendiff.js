ebe87f2216dec40e336843c1f8f0854f04a9c443
v1dgt <v1dgt@mail.ru>
2025-08-17 16:32:14 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,23 +1,34 @@
 #!/usr/bin/env node
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'node:path'
 import { program } from 'commander'
-import { cwd } from 'node:process'
-import { resolve } from 'node:path'
 import parseJson from './gendiff-parse.js'
 import comparison from './gendiff-comparison.js'
 
-program
-  .description('Compares two configuration files and shows a difference')
-  .version('1.0.0')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .option('-f, --format [type]', 'output format (default: "stylish")')
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-  .action((filepath1, filepath2) => {
-    const convertPath = value => value.startsWith('/') ? value : resolve(cwd(), value)
-    const path1 = convertPath(filepath1)
-    const path2 = convertPath(filepath2)
-    const file1 = parseJson(path1)
-    const file2 = parseJson(path2)
-    comparison(file1, file2)
-  })
-program.parse()
+export function gendiff(filepath1, filepath2) {
+  const convertPath = (value) => {
+    if (value.startsWith('/')) return value
+    return resolve(__dirname, '..', value)
+  }
+  const path1 = convertPath(filepath1)
+  const path2 = convertPath(filepath2)
+  const file1 = parseJson(path1)
+  const file2 = parseJson(path2)
+  return comparison(file1, file2)
+}
+
+if (import.meta.url === `file://${process.argv[1]}`) {
+  program
+    .description('Compares two configuration files and shows a difference')
+    .version('1.0.0')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format [type]', 'output format (default: "stylish")')
+    .action((filepath1, filepath2) => {
+      const diff = gendiff(filepath1, filepath2)
+      console.log(diff)
+    })
+  program.parse()
+}

4714769a7d9919091d3d1639c59a299f094ab2b2
v1dgt <v1dgt@mail.ru>
2025-08-28 19:44:49 +0300

    add test in gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,8 +6,19 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 
-test('gendiff', () => {
+test('gendiff json', () => {
   expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(`{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`)
+})
+
+test('gendiff yml', () => {
+  expect(gendiff(getFixturePath('file3.json'), getFixturePath('file4.json'))).toEqual(`{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22

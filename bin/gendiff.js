94e72d4e45bc29712542c0f6c71c6889108a4f49
v1dgt <v1dgt@mail.ru>
2025-09-04 19:05:16 +0300

    upd gendiff and package.json

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,23 +1,6 @@
 #!/usr/bin/env node
-import { fileURLToPath } from 'node:url'
-import { dirname, resolve } from 'node:path'
 import { program } from 'commander'
-import parseData from './parsers.js'
-import comparison from './gendiff-comparison.js'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
-export function gendiff(filepath1, filepath2) {
-  const convertPath = (value) => {
-    if (value.startsWith('/')) return value
-    return resolve(__dirname, '..', value)
-  }
-  const path1 = convertPath(filepath1)
-  const path2 = convertPath(filepath2)
-  const file1 = parseData(path1)
-  const file2 = parseData(path2)
-  return comparison(file1, file2)
-}
+import gendiff from '../src/index.js'
 
 if (import.meta.url === `file://${process.argv[1]}`) {
   program

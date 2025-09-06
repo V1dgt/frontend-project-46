a43b33e27f5554fd2b6fdc43dfbd328eeb8eda62
v1dgt <v1dgt@mail.ru>
2025-09-06 13:39:56 +0300

    upd index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { fileURLToPath } from 'node:url'
 import { dirname, resolve } from 'node:path'
 import parseData from './parsers.js'
 import buildDiff from './compare.js'
+import stylish from './stylish.js'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -14,7 +15,8 @@ const gendiff = (filepath1, filepath2) => {
   const path2 = convertPath(filepath2)
   const file1 = parseData(path1)
   const file2 = parseData(path2)
-  return buildDiff(file1, file2)
+  const diff = buildDiff(file1, file2)
+  return stylish(diff)
 }
 
 export default gendiff

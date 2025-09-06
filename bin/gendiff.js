589112cee5e104d28c4a11dae9443cabe70b7508
v1dgt <v1dgt@mail.ru>
2025-09-06 13:38:56 +0300

    add default stylish in gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,7 @@ if (import.meta.url === `file://${process.argv[1]}`) {
     .version('1.0.0')
     .argument('<filepath1>')
     .argument('<filepath2>')
-    .option('-f, --format [type]', 'output format')
+    .option('-f, --format [type]', 'output format (default: "stylish")', 'stylish')
     .action((filepath1, filepath2) => {
       const diff = gendiff(filepath1, filepath2)
       console.log(diff)

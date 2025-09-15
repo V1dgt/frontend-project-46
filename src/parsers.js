c2b7e1a040e54e78096da4476911c343631a1687
v1dgt <v1dgt@mail.ru>
2025-09-15 17:17:54 +0300

    upd parser

--- src/parsers.js
+++ src/parsers.js
@@ -5,6 +5,7 @@ export default function (path) {
   const file = readFileSync(path, 'utf-8')
   const splitPath = path.split('.')
   const extension = splitPath[splitPath.length - 1]
+
   if (extension === 'json') {
     return JSON.parse(file)
   }

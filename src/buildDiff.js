4686457f83ecda800e6208d32b4458a829bdb251
v1dgt <v1dgt@mail.ru>
2025-09-16 13:25:54 +0300

    upd buildDiff

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -6,10 +6,10 @@ const buildDiff = (file1, file2) => {
   const allKeys = [...Object.keys(file1), ...Object.keys(file2)]
   const sortedUniqueKeys = sortBy([...new Set(allKeys)])
   return sortedUniqueKeys.map((key) => {
-    if (!Object.hasOwn(file2, key)) {
+    if (!Object.prototype.hasOwnProperty.call(file2, key)) {
       return { key, type: 'removed', value: file1[key] }
     }
-    if (!Object.hasOwn(file1, key)) {
+    if (!Object.prototype.hasOwnProperty.call(file1, key)) {
       return { key, type: 'added', value: file2[key] }
     }
     if (isObject(file1[key]) && isObject(file2[key])) {

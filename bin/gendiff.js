0b08986992000cbc81da7b4cee6132cf22a20e5e
v1dgt <v1dgt@mail.ru>
2025-09-17 15:41:31 +0300

    upd version commander in gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { program } from 'commander'
+import genDiff from '../src/index.js'
+
+if (import.meta.url === `file://${process.argv[1]}`) {
+  program
+    .description('Compares two configuration files and shows a difference')
+    .version('0.0.1')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format [type]', 'output format (default: "stylish")', 'stylish')
+    .action((filepath1, filepath2, options) => {
+      const diff = genDiff(filepath1, filepath2, options.format)
+      console.log(diff)
+    })
+
+  program.parse()
+}

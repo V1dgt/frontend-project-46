c2a73d5a6fdd37bc0ce9fbbe9a2214eff27cd886
v1dgt <v1dgt@mail.ru>
2025-09-17 16:33:23 +0300

    upd gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,12 +2,14 @@
 import { program } from 'commander'
 import genDiff from '../src/index.js'
 
-program
-  .version('0.0.1')
-  .arguments('<filepath1> <filepath2>')
-  .description('Compares two configuration files and shows a difference')
-  .option('-f, --format [type]', 'output format (default: "stylish")', 'stylish')
-  .action((path1, path2) => {
-    console.log(genDiff(path1, path2, program.opts().format))
-  })
-  .parse(process.argv)
+if (import.meta.url === `file://${process.argv[1]}`) {
+  program
+    .version('0.0.1')
+    .arguments('<filepath1> <filepath2>')
+    .description('Compares two configuration files and shows a difference')
+    .option('-f, --format [type]', 'output format (default: "stylish")', 'stylish')
+    .action((path1, path2) => {
+      console.log(genDiff(path1, path2, program.opts().format))
+    })
+    .parse(process.argv)
+}
